8c992053e9f66a6cdaf95cdd3403bed7a267c136
eike012 <eike_souza@hotmail.com>
2025-09-02 18:26:35 -0300

    feat: add repository interface for product entity

--- backend/src/Hypesoft.Application/obj/Debug/net9.0/Hypesoft.Application.AssemblyInfo.cs
+++ backend/src/Hypesoft.Application/obj/Debug/net9.0/Hypesoft.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hypesoft.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d79a4b4369b30e9f2c6075f70cda6a0b4cbe1652")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968a166b7054fe332d8ba25e6ee0006ebaf47d30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hypesoft.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hypesoft.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ backend/src/Hypesoft.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hypesoft.Domain.Repositories
+{
+   public interface IProductRepository
+   {
+     Task<Product> GetByIdAsync(int id);
+     Task<IEnumerable<Product>> GetAllAsync();
+     Task AddAsync(Product product);
+     Task UpdateAsync(Product product);
+     Task DeleteAsync(int id);
+   }
+}
